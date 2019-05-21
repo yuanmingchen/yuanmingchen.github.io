36fa42f7324cbede9dfcaf4f37ba7b8a8e128a2d
yuanmc <438353531@qq.com>
2019-05-21 16:51:18 +0800

    Track 1 files into repository.
    - modified assets/themes/mz/js/jquery.toc.js
    
    Auto commit by GitBook Editor

--- assets/themes/mz/js/jquery.toc.js
+++ assets/themes/mz/js/jquery.toc.js
@@ -174,9 +174,8 @@ $(document).ready(function(){
 });
 
 $(".jumper").on("click", function( e ) {
-  //e.preventDefault();
-  //$("body, html").animate({
-  //  scrollTop: $( $(this).attr('href') ).offset().top
-  //}, 600);
-  alert("-----------");
+  e.preventDefault();
+  $("body, html").animate({
+    scrollTop: $( $(this).attr('href') ).offset().top
+  }, 600);
 });
