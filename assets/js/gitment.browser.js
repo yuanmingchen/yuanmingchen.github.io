c6e473ba3c54d306e64666d3fdd5749b70459d6d
袁明琛 <yuanmingchen@yuanmingchendeMacBook-Pro.local>
2020-03-11 10:57:39 +0800

    change the gitemnt server

--- assets/js/gitment.browser.js
+++ assets/js/gitment.browser.js
@@ -3116,7 +3116,7 @@ function renderFooter() {
   var container = document.createElement('div');
   container.lang = "en-US";
   container.className = 'gitment-container gitment-footer-container';
-  container.innerHTML = '\n    Powered by\n    <a class="gitment-footer-project-link" href="https://github.com/imsun/gitment" target="_blank">\n      Gitment\n    </a>\n  ';
+  container.innerHTML = '\n    Powered by\n    <a class="gitment-footer-project-link" href="https://gitment-awen.herokuapp.com" target="_blank">\n      Gitment\n    </a>\n  ';
   return container;
 }
 
